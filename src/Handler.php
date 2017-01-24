97b7ff85ed6e40f0dc9170f53f97396253f81172
Dieter Blomme <dieterblomme@gmail.com>
2017-01-25 00:23:57 +0100

    Initial commit

---
+++ src/Handler.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * @file
+ * Contains \DrupalComposer\DrupalCopyProfile\Handler.
+ */
+
+namespace DrupalComposer\DrupalCopyProfile;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Util\Filesystem;
+use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
+
+class Handler {
+
+  /**
+   * @var \Composer\Composer
+   */
+  protected $composer;
+
+  /**
+   * @var \Composer\IO\IOInterface
+   */
+  protected $io;
+
+  /**
+   * Handler constructor.
+   *
+   * @param Composer $composer
+   * @param IOInterface $io
+   */
+  public function __construct(Composer $composer, IOInterface $io) {
+    $this->composer = $composer;
+    $this->io = $io;
+  }
+
+  /**
+   * Copy profile command event to copy the profile.
+   *
+   * @param \Composer\Script\Event $event
+   */
+  public function copyProfile(\Composer\Script\Event $event) {
+    $filesystem = new Filesystem();
+    $symfonyfilesystem = new SymfonyFilesystem();
+    $webroot = realpath($this->getWebRoot());
+    $root = realpath(getcwd());
+    $profile_dir = $webroot . '/profiles/contrib/' . $this->composer->getPackage()->getName();
+
+    $excludes = $this->getExcludesDefault();
+
+    // If the profile isn't there: create the folder, else, empty it.
+    $filesystem->remove($profile_dir);
+    $filesystem->ensureDirectoryExists($profile_dir);
+
+    $iterator = self::getRecursiveIteratorIterator($excludes);
+    $symfonyfilesystem->mirror($root, $profile_dir, $iterator);
+  }
+
+  /**
+   * Get the path to the 'vendor' directory.
+   *
+   * @return string
+   */
+  public function getVendorPath() {
+    $config = $this->composer->getConfig();
+    $filesystem = new Filesystem();
+    $filesystem->ensureDirectoryExists($config->get('vendor-dir'));
+    $vendorPath = $filesystem->normalizePath(realpath($config->get('vendor-dir')));
+
+    return $vendorPath;
+  }
+
+  /**
+   * Look up the Drupal core package object.
+   *
+   * @return PackageInterface
+   */
+  public function getDrupalCorePackage() {
+    return $this->getPackage('drupal/core');
+  }
+
+  /**
+   * Retrieve the path to the web root.
+   *
+   *  @return string
+   */
+  public function getWebRoot() {
+    $drupalCorePackage = $this->getDrupalCorePackage();
+    $installationManager = $this->composer->getInstallationManager();
+    $corePath = $installationManager->getInstallPath($drupalCorePackage);
+    // Webroot is the parent path of the drupal core installation path.
+    $webroot = dirname($corePath);
+
+    return $webroot;
+  }
+
+  /**
+   * Retrieve a package from the current composer process.
+   *
+   * @param string $name
+   *   Name of the package to get from the current composer installation.
+   *
+   * @return PackageInterface
+   */
+  protected function getPackage($name) {
+    return $this->composer->getRepositoryManager()->getLocalRepository()->findPackage($name, '*');
+  }
+
+  /**
+   * Retrieve excludes from optional "extra" configuration.
+   *
+   * @return array
+   */
+  protected function getExcludes() {
+    return $this->getNamedOptionList('excludes', 'getExcludesDefault');
+  }
+
+  /**
+   * Retrieve a named list of options from optional "extra" configuration.
+   * Respects 'omit-defaults', and either includes or does not include the
+   * default values, as requested.
+   *
+   * @return array
+   */
+  protected function getNamedOptionList($optionName, $defaultFn) {
+    $options = $this->getOptions();
+    $result = array();
+    if (empty($options['omit-defaults'])) {
+      $result = $this->$defaultFn();
+    }
+    $result = array_merge($result, (array) $options[$optionName]);
+
+    return $result;
+  }
+
+  /**
+   * Holds default excludes.
+   */
+  protected function getExcludesDefault() {
+    $common = [
+      $this->getWebRoot(),
+      $this->getVendorPath(),
+      '.git',
+    ];
+
+    sort($common);
+    return $common;
+  }
+
+  /**
+   * Retrieve excludes from optional "extra" configuration.
+   *
+   * @return array
+   */
+  protected function getOptions() {
+    $extra = $this->composer->getPackage()->getExtra() + ['drupal-copy-profile' => []];
+    $options = $extra['drupal-copy-profile'] + [
+        'omit-defaults' => FALSE,
+        'excludes' => [],
+        'web-root' => $this->getWebRoot(),
+      ];
+    return $options;
+  }
+
+}
---
+++ src/Plugin.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @file
+ * Contains DrupalComposer\DrupalCopyProfile\Plugin.
+ */
+
+namespace DrupalComposer\DrupalCopyProfile;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+
+/**
+ * Composer plugin for handling drupal scaffold.
+ */
+class Plugin implements PluginInterface, EventSubscriberInterface {
+
+  /**
+   * @var \DrupalComposer\DrupalCopyProfile\Handler
+   */
+  protected $handler;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    // We use a separate PluginScripts object. This way we separate
+    // functionality and also avoid some debug issues with the plugin being
+    // copied on initialisation.
+    // @see \Composer\Plugin\PluginManager::registerPackage()
+    $this->handler = new Handler($composer, $io);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return array(
+      ScriptEvents::POST_INSTALL_CMD => 'copyProfile',
+      ScriptEvents::POST_UPDATE_CMD => 'copyProfile',
+    );
+  }
+
+  /**
+   * Copy profile event callback. Can be used as postCmdCallback and Script callback to be put in composer scripts.
+   *
+   * @param \Composer\Script\Event $event
+   */
+  public function copyProfile(\Composer\Script\Event $event) {
+    if ($this->handler === null) {
+      $handler = new Handler($event->getComposer(), $event->getIO());
+    }
+    $this->handler->copyProfile($event);
+  }
+}
